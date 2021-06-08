7169c11a765343486070d1e1e91e4f292f8a8c9f
Jonathan Neidel <jneidel@jneidel.com>
2021-06-08 15:26:17 +0200

    Fix json being split on space

--- format.sh
+++ format.sh
@@ -8,8 +8,8 @@ JOB_TITLES=$( cat $JOB_TITLES_FILE | tr A-Z a-z | sed -e "s/-/ /g" | sed -e "s/,
 printf '%b\n' "$JOB_TITLES" > $JOB_TITLES_FILE
 
 echo "Generating: $JOB_TITLES_JSON_FILE"
-JOB_TITLES_JSON=$(printf '%b\n' $JOB_TITLES | sed -e 's/\(.*\)/    "\1",/' | sed "$ s/.$//")
-JOB_TITLES_JSON="{\n  \"job-titles\": [\n${JOB_TITLES_JSON}\n  ]\n}"
+JOB_TITLES_JSON=$(printf '%b\n' "$JOB_TITLES" | awk -F\; '{ print "\""$1"\"," }' | sed "$ s/.$//")
+JOB_TITLES_JSON="{\"job-titles\": [\n${JOB_TITLES_JSON}\n] }"
 printf '%b\n' "$JOB_TITLES_JSON" > $JOB_TITLES_JSON_FILE
 
 echo "Updating readme item counter"
@@ -17,4 +17,3 @@ README=readme.md
 JOB_TITLES_LENGTH=$(echo "$JOB_TITLES" | wc -l)
 README_CONTENT=$(cat $README | sed -e "s|/job_titles-[0-9]\+-|/job_titles-$JOB_TITLES_LENGTH-|g")
 printf '%b\n' "$README_CONTENT" > $README
-
